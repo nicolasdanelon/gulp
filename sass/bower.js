7597b3cbbabd7986c39fe2910a06a63de5055850
Nicolas Danelon <nicolasdanelon@users.noreply.github.com>
2016-01-21 15:41:32 -0300

    Create bower.js

---
+++ sass/bower.js
@@ -0,0 +1,25 @@
+{
+  "name": "###",
+  "description": "Tasks for sass, boostrap & js min",
+  "main": "index.js",
+  "authors": [
+    "Nicolas Danelon <mail@mail>",
+    "name2 <mail2@mail>"
+  ],
+  "license": "ISC",
+  "moduleType": [],
+  "homepage": "",
+  "private": true,
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "devDependencies": {
+    "circles": "*",
+    "bootstrap-sass": "~3.3.6",
+    "font-awesome-bower": "~4.4.0"
+  }
+}
